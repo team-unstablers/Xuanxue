af3203bfb0be9e389d65a82329d0e2919943c9fb
Gyuhwan Park★ <unstabler@unstabler.pl>
2026-01-18 16:05:48 +0900

    Package.swift: swift-asn1의 버전을 1.5.1로 올림

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.5.1"),
     ],
     targets: [
         .target(
