eecd56e4c3d00f7b628fe309958dba75c4924714
Gyuhwan Park★ <unstabler@unstabler.pl>
2026-01-29 00:26:24 +0900

    libbcrypt를 타겟으로 expose함

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "Xuanxue",
-            targets: ["Xuanxue"]
+            targets: ["Xuanxue", "libbcrypt"]
         ),
     ],
     dependencies: [
